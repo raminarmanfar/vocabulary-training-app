7b153b4da1b6a71992003694172bdfd6137cd8f7
Ramin Armanfar <ramin.armanfar@gmail.com>
2026-05-21 11:58:30 +0200

    feat: handle Android hardware back button - navigate to dashboard, exit app from dashboard

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Component, OnInit, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonApp, IonRouterOutlet, Platform } from '@ionic/angular/standalone';
+import { App as CapApp } from '@capacitor/app';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
   template: '<ion-app><ion-router-outlet></ion-router-outlet></ion-app>',
 })
-export class App {}
+export class App implements OnInit {
+  private platform = inject(Platform);
+  private router = inject(Router);
+
+  ngOnInit() {
+    this.platform.backButton.subscribeWithPriority(10, () => {
+      const currentUrl = this.router.url;
+      if (currentUrl === '/dashboard' || currentUrl === '/') {
+        CapApp.exitApp();
+      } else {
+        this.router.navigate(['/dashboard']);
+      }
+    });
+  }
+}
