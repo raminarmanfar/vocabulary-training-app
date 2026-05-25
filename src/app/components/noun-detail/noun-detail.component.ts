d1dfda7d7ea57f18406a455d7473740637ffcf4d
Ramin Armanfar <ramin.armanfar@gmail.com>
2026-05-25 17:44:53 +0200

    feat: add pronunciation button for noun plural form

--- src/app/components/noun-detail/noun-detail.component.ts
+++ src/app/components/noun-detail/noun-detail.component.ts
@@ -1,11 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import {
   IonCard, IonCardHeader, IonCardTitle, IonCardContent,
   IonItem, IonLabel, IonBadge, IonGrid, IonRow, IonCol,
-  IonAccordionGroup, IonAccordion
+  IonAccordionGroup, IonAccordion, IonButton, IonIcon
 } from '@ionic/angular/standalone';
 import { TranslatePipe } from '@ngx-translate/core';
+import { addIcons } from 'ionicons';
+import { volumeHighOutline } from 'ionicons/icons';
 import { NounDetails } from '../../models/vocabulary.model';
+import { TtsService } from '../../services/tts.service';
 
 @Component({
   selector: 'app-noun-detail',
@@ -15,12 +18,23 @@ import { NounDetails } from '../../models/vocabulary.model';
   imports: [
     IonCard, IonCardHeader, IonCardTitle, IonCardContent,
     IonItem, IonLabel, IonBadge, IonGrid, IonRow, IonCol,
-    IonAccordionGroup, IonAccordion,
+    IonAccordionGroup, IonAccordion, IonButton, IonIcon,
     TranslatePipe
   ]
 })
 export class NounDetailComponent {
   @Input() details!: NounDetails;
+
+  private tts = inject(TtsService);
+
+  constructor() {
+    addIcons({ volumeHighOutline });
+  }
+
+  async speakPlural(event: Event) {
+    event.stopPropagation();
+    await this.tts.speak(this.details.plural);
+  }
 
   cases = ['nominative', 'akkusativ', 'genitiv', 'dativ'] as const;
   caseKeys: Record<string, string> = {
